dc9120426cfa0c540e6ac1709a91b445cd61644a
Hugo Lara <hugo.alex200197@outlook.com>
2020-08-27 18:13:52 -0500

    Referencia a boton

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const celeste = document.getElementById('celeste');
 const violeta = document.getElementById('violeta');
 const naranja = document.getElementById('naranja');
 const verde = document.getElementById('verde');
+const btnEmpezar = document.getElementById('btnEmpezar')
 
 class Juego{
     constructor(){
