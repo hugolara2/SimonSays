fea17265f3f6df7a0eb14f5b469fba84376f6b8e
Hugo Lara <hugo.alex200197@outlook.com>
2020-08-27 18:11:00 -0500

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const celeste = document.getElementById('celeste');
+const violeta = document.getElementById('violeta');
+const naranja = document.getElementById('naranja');
+const verde = document.getElementById('verde');
+
+class Juego{
+    constructor(){
+        this.inicializar();
+    }
+
+    inicializar(){
+        btnEmpezar.classList.add('hide');
+    }
+}
+
+function empezar(){
+    var juego = new Juego();
+}
