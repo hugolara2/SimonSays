38cd72500c99b59b318836fe63b6291ae19d7d36
Hugo Lara <hugo.alex200197@outlook.com>
2020-08-27 23:11:43 -0500

    Probando arreglar mis cosas

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
-const celeste = document.getElementById('celeste');
-const violeta = document.getElementById('violeta');
-const naranja = document.getElementById('naranja');
-const verde = document.getElementById('verde');
-const btnEmpezar = document.getElementById('btnEmpezar')
+const celeste = document.getElementById("celeste");
+const violeta = document.getElementById("violeta");
+const naranja = document.getElementById("naranja");
+const verde = document.getElementById("verde");
+const btnEmpezar = document.getElementById("btnEmpezar");
 
 class Juego{
     constructor(){
         this.inicializar();
+        this.generarSecuencia();
     }
 
     inicializar(){
         btnEmpezar.classList.add('hide');
     }
+
+    generarSecuencia(){
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+    }
+}
+
+function Esta(constante){
+    if(constante){
+        return 1;
+    }else{
+        return 0;
+    }
 }
 
 function empezar(){
-    var juego = new Juego();
+    window.juego = new Juego();
+
 }
